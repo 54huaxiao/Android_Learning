f7ab67ca4825ebf5a91649586448c598ee0c92cf
MoHuaxiao <54huaxiao@gmail.com>
2016-10-03 00:16:39 +0800

    update hw1

---
+++ lab1_code/app/src/main/java/com/example/vincent/week1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vincent.week1;
+
+/**
+ * Created by Vincent on 2016/9/10.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+}
